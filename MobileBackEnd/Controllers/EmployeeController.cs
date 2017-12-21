597d9570b20ba8b8ea1c67f627eee7e807578c8b
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2017-12-21 22:29:40 +0200

    workx

---
+++ MobileBackEnd/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using MobileBackEnd.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileBackEnd.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public string[] GetAll()
+        {
+            string[] employeeNames = null;
+            TimeSheetEntities entities = new TimeSheetEntities();
+            try
+            {
+              employeeNames = (from e in entities.Employees
+                               where (e.Active == true)
+                               select e.FirstName+" "+e.LastName).ToArray();
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+            return employeeNames;    
+        }          
+    }
+}
---
+++ MobileBackEnd/Controllers/WorkAssignmentController.cs
@@ -0,0 +1,30 @@
+﻿using MobileBackEnd.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileBackEnd.Controllers
+{
+    public class WorkAssignmentController : ApiController
+    {
+        public string[] GetAll()
+        {
+            string[] assignmentNames = null;
+            TimeSheetEntities entities = new TimeSheetEntities();
+            try
+            {
+                assignmentNames = (from wa in entities.WorkAssignments
+                                 where (wa.Active == true)
+                                 select wa.Title).ToArray();
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+            return assignmentNames;
+        }
+    }
+}
