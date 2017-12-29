625656693c3a9b231ab167076fb0554cf8d8e5d3
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2017-12-29 22:14:47 +0200

    Toimii

--- MobileBackEnd/Controllers/WorkAssignmentController.cs
+++ MobileBackEnd/Controllers/WorkAssignmentController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TimeSheetMobile.Models;
 
 namespace MobileBackEnd.Controllers
 {
@@ -25,6 +26,70 @@ namespace MobileBackEnd.Controllers
                 entities.Dispose();
             }
             return assignmentNames;
+        }
+
+        [HttpPost]
+        public bool PostStatus(WorkAssignmentOperationModel input)
+        {
+            TimeSheetEntities entities = new TimeSheetEntities();
+            try
+            {
+                WorkAssignments assignment = (from wa in entities.WorkAssignments
+                                             where (wa.Active == true) &&
+                                             (wa.Title == input.AssignmentTitle)
+                                             select wa).FirstOrDefault();
+
+                if (assignment == null)
+                {
+                    return false;
+                }
+
+                if (input.Operation == "Start")
+                {
+                    int assignmentId = assignment.Id_WorkAssignment;
+                    Timesheet newEntry = new Timesheet()
+                    {
+                        Id_WorkAssignment = assignmentId,
+                        StartTime = DateTime.Now,
+                        WorkComplete = false,
+                        Active = true,
+                        CreatedAt = DateTime.Now
+                    };
+                    entities.Timesheet.Add(newEntry);
+                }
+                else if (input.Operation == "Stop")
+                {
+                    int assignmentId = assignment.Id_WorkAssignment;
+                    Timesheet existing = (from ts in entities.Timesheet
+                                          where (ts.Id_WorkAssignment == assignmentId) &&
+                                          (ts.Active == true) && (ts.WorkComplete == false)
+                                          orderby ts.StartTime descending
+                                          select ts).FirstOrDefault();
+
+                    if (existing != null)
+                    {
+                        existing.StopTime = DateTime.Now;
+                        existing.WorkComplete = true;
+                        existing.LastModifiedAt = DateTime.Now;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                entities.SaveChanges();
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            return true;
         }
     }
 }
