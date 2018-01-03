1ab4893f92751143681a56c612315ee1efe61086
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2018-01-03 19:19:51 +0200

    Toimii kaavio.

--- MobileBackEnd/Controllers/ReportsController.cs
+++ MobileBackEnd/Controllers/ReportsController.cs
@@ -1,8 +1,10 @@
-﻿using MobileBackEnd.DataAccess;
+﻿using MobileBackend.ViewModels;
+using MobileBackEnd.DataAccess;
 using MobileBackEnd.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -55,6 +57,85 @@ namespace MobileBackEnd.Controllers
                 entities.Dispose();
             }
           
+        }
+        public ActionResult HoursPerWorkAssignmentAsExcel()
+        {
+            //TODO:hae tiedot tietokanasta
+            StringBuilder csv = new StringBuilder();
+
+            //luodaan CSV-muotoinen tiedosto
+            csv.AppendLine("MAtti;123,3");
+            csv.AppendLine("Jesse;123,3");
+            csv.AppendLine("Kaisa;123,3");
+            // palautetaan CSV-tiedot selaimelle
+            byte[] buffer = Encoding.UTF8.GetBytes(csv.ToString());
+            return File(buffer, "text/csv", "Työtunnit.csv");
+        }
+        public ActionResult HoursPerWorkAssignmentAsExcel2()
+        {
+     
+            StringBuilder csv = new StringBuilder();
+
+            //luodaan CSV-muotoinen tiedosto
+            TimeSheetEntities entities = new TimeSheetEntities();
+            try
+            {
+                DateTime today = DateTime.Today;
+                DateTime tomorrow = today.AddDays(1);
+           
+            //haetaan kuluvan päivän tuntikirjaukset
+            List<Timesheet> allTimesheetsToday = (from ts in entities.Timesheet
+                                                  where (ts.StartTime > today) &&
+                                                  (ts.StartTime < tomorrow) && (ts.WorkComplete == true)
+                                                  select ts).ToList();
+                foreach (Timesheet timesheet in allTimesheetsToday)
+                {
+                    csv.AppendLine(timesheet.Id_Employee + "," + timesheet.StartTime + "," + timesheet.StopTime + ",");
+                }
+ }
+
+           finally
+            {
+                entities.Dispose();
+            }
+            // palautetaan CSV-tiedot selaimelle
+            byte[] buffer = Encoding.UTF8.GetBytes(csv.ToString());
+            return File(buffer, "text/csv", "Työtunnit.csv");
+        }
+        public ActionResult GetTimesheetCounts(string onlyComplete)
+        {
+            ReportChartDataViewModel model = new ReportChartDataViewModel();
+
+            TimeSheetEntities entities = new TimeSheetEntities();
+            try
+            {
+                model.Labels = (from wa in entities.WorkAssignments
+                                orderby wa.Id_WorkAssignment
+                                select wa.Title).ToArray();
+
+                if (onlyComplete == "1")
+                {
+                    model.Counts = (from ts in entities.Timesheet
+                                    where (ts.WorkComplete == true)
+                                    orderby ts.Id_WorkAssignment
+                                    group ts by ts.Id_WorkAssignment into grp
+                                    select grp.Count()).ToArray();
+                }
+                else
+                {
+                    model.Counts = (from ts in entities.Timesheet
+                                    orderby ts.Id_WorkAssignment
+                                    group ts by ts.Id_WorkAssignment into grp
+                                    select grp.Count()).ToArray();
+                }
+
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ MobileBackEnd/ViewModels/ReportChartDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileBackend.ViewModels
+{
+    public class ReportChartDataViewModel
+    {
+        public string[] Labels { get; set; }
+        public int[] Counts { get; set; }
+    }
+}
